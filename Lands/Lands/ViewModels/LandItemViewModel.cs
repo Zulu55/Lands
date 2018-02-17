c0b3238d24ca95ad4a439a4eaf9ab9605bde1f48
Cayman01 <cayman01.filiales@gmail.com>
2018-02-17 08:36:25 -0500

    Tabbed page

--- Lands/Lands/ViewModels/LandItemViewModel.cs
+++ Lands/Lands/ViewModels/LandItemViewModel.cs
@@ -20,7 +20,7 @@
         private async void SelectLand()
         {
             MainViewModel.GetInstance().Land = new LandViewModel(this);
-            await Application.Current.MainPage.Navigation.PushAsync(new LandPage());
+            await Application.Current.MainPage.Navigation.PushAsync(new LandTabbedPage());
         }
         #endregion
     }
---
+++ Lands/Lands/Views/LanguagesPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Lands.Views
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LanguagesPage : ContentPage
+	{
+		public LanguagesPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
