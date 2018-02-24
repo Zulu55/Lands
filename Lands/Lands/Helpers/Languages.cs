8b2ae86000ace7c7e97d405be1e503bfa2f46b16
cayman01 <cayman01.filiales@gmail.com>
2018-02-24 17:02:35 -0500

    Literals on login page

--- Lands/Lands/Helpers/Languages.cs
+++ Lands/Lands/Helpers/Languages.cs
@@ -47,5 +47,35 @@
         {
             get { return Resource.SomethingWrong; }
         }
+
+        public static string Login
+        {
+            get { return Resource.Login; }
+        }
+
+        public static string EMail
+        {
+            get { return Resource.EMail; }
+        }
+
+        public static string Password
+        {
+            get { return Resource.Password; }
+        }
+
+        public static string PasswordPlaceHolder
+        {
+            get { return Resource.PasswordPlaceHolder; }
+        }
+
+        public static string Forgot
+        {
+            get { return Resource.Forgot; }
+        }
+
+        public static string Register
+        {
+            get { return Resource.Register; }
+        }
     }
 }
