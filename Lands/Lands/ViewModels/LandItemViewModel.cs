e5ede1af83f391e60967ab6022e0360dbc292084
Cayman01 <cayman01.filiales@gmail.com>
2018-03-03 07:58:54 -0500

    Fix navigation

--- Lands/Lands/ViewModels/LandItemViewModel.cs
+++ Lands/Lands/ViewModels/LandItemViewModel.cs
@@ -20,7 +20,7 @@
         private async void SelectLand()
         {
             MainViewModel.GetInstance().Land = new LandViewModel(this);
-            await Application.Current.MainPage.Navigation.PushAsync(new LandTabbedPage());
+            await App.Navigator.PushAsync(new LandTabbedPage());
         }
         #endregion
     }
