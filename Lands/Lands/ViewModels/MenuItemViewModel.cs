608159c68740062cffd54eca45a1c63180e0fb6d
cayman01 <cayman01.filiales@gmail.com>
2018-03-10 06:25:33 -0500

    Icon menu

--- Lands/Lands/ViewModels/MenuItemViewModel.cs
+++ Lands/Lands/ViewModels/MenuItemViewModel.cs
@@ -34,7 +34,8 @@
                 var mainViewModel = MainViewModel.GetInstance();
                 mainViewModel.Token = string.Empty;
                 mainViewModel.TokenType = string.Empty;
-                Application.Current.MainPage = new LoginPage();
+                Application.Current.MainPage = new NavigationPage(
+                    new LoginPage());
             }
         }
         #endregion
