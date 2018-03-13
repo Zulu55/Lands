78985c570e97cde594e8837a93bafdafd8270494
Cayman01 <cayman01.filiales@gmail.com>
2018-03-12 22:41:27 -0500

    xxx

--- Lands/Lands.Android/Resources/Resource.designer.cs
+++ Lands/Lands.Android/Resources/Resource.designer.cs
@@ -26,6 +26,8 @@ namespace Lands.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.ApplicationName = global::Lands.Droid.Resource.String.ApplicationName;
+			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.Hello = global::Lands.Droid.Resource.String.Hello;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::Lands.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -3669,6 +3671,12 @@ namespace Lands.Droid
 		
 		public partial class String
 		{
+			
+			// aapt resource value: 0x7f09003f
+			public const int ApplicationName = 2131296319;
+			
+			// aapt resource value: 0x7f09003e
+			public const int Hello = 2131296318;
 			
 			// aapt resource value: 0x7f090015
 			public const int abc_action_bar_home_description = 2131296277;
