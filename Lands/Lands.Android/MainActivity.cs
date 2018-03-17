23b2088011b6ee83ed393dfff0952c664ff94215
Cayman01 <cayman01.filiales@gmail.com>
2018-03-17 06:32:28 -0500

    Camera on Android

--- Lands/Lands.Android/MainActivity.cs
+++ Lands/Lands.Android/MainActivity.cs
@@ -3,9 +3,15 @@
     using Android.App;
     using Android.Content.PM;
     using Android.OS;
+    using Android.Runtime;
     using FFImageLoading.Forms.Droid;
+    using Plugin.Permissions;
 
-    [Activity(Label = "Lands", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(
+        Label = "Lands",
+        Icon = "@drawable/icon",
+        Theme = "@style/MainTheme",
+        MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -18,6 +24,17 @@
             global::Xamarin.Forms.Forms.Init(this, bundle);
             CachedImageRenderer.Init(true);
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(
+            int requestCode, 
+            string[] permissions, 
+            [GeneratedEnum] Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(
+                requestCode, 
+               permissions, 
+               grantResults);
         }
     }
 }
