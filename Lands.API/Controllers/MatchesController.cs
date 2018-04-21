1d772c76bd24147933d9e9882c9d6698e27a662f
Geralin fernandez <gfernan6@eafit.edu.co>
2018-04-21 08:25:55 -0500

    Match API Controller

---
+++ Lands.API/Controllers/MatchesController.cs
@@ -0,0 +1,59 @@
+﻿namespace Lands.API.Controllers
+{
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using Domain;
+    using Models;
+    using System.Collections.Generic;
+
+    [Authorize]
+    public class MatchesController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/Matches
+        public async Task<IHttpActionResult> GetMatches()
+        {
+            var responses = new List<MatchResponse>();
+            var matches = await db.Matches.ToListAsync();
+
+            foreach (var match in matches)
+            {
+                responses.Add(new MatchResponse
+                {
+                    DateTime = match.DateTime,
+                    Group = match.Group,
+                    GroupId = match.GroupId,
+                    Local = match.Local,
+                    LocalGoals = match.LocalGoals,
+                    LocalId = match.LocalId,
+                    MatchId = match.MatchId,
+                    Predictions = match.Predictions.ToList(),
+                    StatusMatch = match.StatusMatch,
+                    StatusMatchId = match.StatusMatchId,
+                    Visitor = match.Visitor,
+                    VisitorGoals = match.VisitorGoals,
+                    VisitorId = match.VisitorId,
+                });
+            }
+
+            return Ok(responses);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MatchExists(int id)
+        {
+            return db.Matches.Count(e => e.MatchId == id) > 0;
+        }
+    }
+}
---
+++ Lands.API/Models/MatchResponse.cs
@@ -0,0 +1,35 @@
+﻿namespace Lands.API.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using Domain;
+
+    public class MatchResponse
+    {
+        public int MatchId { get; set; }
+
+        public int GroupId { get; set; }
+
+        public int LocalId { get; set; }
+
+        public int VisitorId { get; set; }
+
+        public int StatusMatchId { get; set; }
+
+        public int? LocalGoals { get; set; }
+
+        public int? VisitorGoals { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public Group Group { get; set; }
+
+        public Team Local { get; set; }
+
+        public Team Visitor { get; set; }
+
+        public StatusMatch StatusMatch { get; set; }
+
+        public List<Prediction> Predictions { get; set; }
+    }
+}
